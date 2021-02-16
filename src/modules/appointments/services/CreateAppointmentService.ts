e63819a81fa180a60aad2d1133e6f01475fc4078
Luiz Henrique Clazzer <luizhclazzer@gmail.com>
2021-02-16 05:50:55 -0300

    Controllers for appointment availability functionality

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { isBefore, startOfHour, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -35,6 +35,20 @@ class CreateAppointmentServices {
 
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already booked');
+    }
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date");
+    }
+
+    if (provider_id === user_id) {
+      throw new AppError("You can't create an appointment with yourself");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create appointments between 8am and 5pm',
+      );
     }
 
     const appointment = await this.appointmentsRepository.create({
