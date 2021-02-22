dd64182c7841938da09b28afd1ada1bd75c77c98
Luiz Henrique Clazzer <luizhclazzer@gmail.com>
2021-02-22 06:15:55 -0300

    Working with enviroment variables

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
-   jwt: {
-     secret: 'bcfadfdb3208b8dac61a500231857f5b',
-     expiresIn: '1d',
-   },
+  jwt: {
+    secret: process.env.APP_SECRET,
+    expiresIn: '1d',
+  },
 };
